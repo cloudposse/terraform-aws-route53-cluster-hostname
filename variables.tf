d171c15737b1732e581b6fb9bd71242aa61bb9d5
Stefan Sedich <stefan.sedich@gmail.com>
2019-06-11 11:36:32 -0700

    Be explicit with types

--- variables.tf
+++ variables.tf
@@ -1,22 +1,27 @@
 variable "enabled" {
+  type        = bool
   description = "Set to false to prevent the module from creating any resources"
-  default     = "true"
+  default     = true
 }
 
 variable "namespace" {
+  type        = string
   description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
+  type        = string
   description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
   default     = "dns"
 }
 
 variable "zone_id" {
+  type        = string
   default     = ""
   description = "Route53 DNS Zone id"
 }
@@ -27,11 +32,13 @@ variable "records" {
 }
 
 variable "type" {
+  type        = string
   default     = "CNAME"
   description = "Type"
 }
 
 variable "ttl" {
+  type        = string
   default     = "300"
   description = "The TTL of the record to add to the DNS zone to complete certificate validation"
 }
