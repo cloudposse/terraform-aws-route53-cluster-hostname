02d082d39ca446fb099ec4e78903eb1358644b67
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:16 +0530

    upgraded-tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_route53_record" "default" {
-  count   = "${var.enabled == "true" ? 1 : 0}"
-  zone_id = "${var.zone_id}"
-  name    = "${var.name}"
-  type    = "${var.type}"
-  ttl     = "${var.ttl}"
-  records = ["${var.records}"]
+  count   = var.enabled == "true" ? 1 : 0
+  zone_id = var.zone_id
+  name    = var.name
+  type    = var.type
+  ttl     = var.ttl
+  records = var.records
 }
+
