abd24152b4b9acd4a1f595db3591449139e03b6e
Hugo Samayoa <htplbc@gmail.com>
2020-09-09 10:15:32 -0700

    Add dns_name so that you can set the hostname externally.  (#27)
    * Add var.dns_name so that you can set the hostname externally. Default to module.this.name from context.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "default" {
   count   = module.this.enabled ? 1 : 0
-  name    = module.this.id
+  name    = var.dns_name == "" ? module.this.id : var.dns_name
   zone_id = var.zone_id
   type    = var.type
   ttl     = var.ttl
--- variables.tf
+++ variables.tf
@@ -19,3 +19,9 @@ variable "ttl" {
   default     = 300
   description = "The TTL of the record to add to the DNS zone to complete certificate validation"
 }
+
+variable "dns_name" {
+  type        = string
+  description = "The name of the DNS record"
+  default     = ""
+}
